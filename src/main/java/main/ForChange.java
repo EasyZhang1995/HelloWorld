a9f2c7c3fec018daf7f0895fe8eab1765c369edf
EasyZhang1995 <452142549@qq.com>
2022-03-31 23:18:28 +0800

    test2

--- src/main/java/main/ForChange.java
+++ src/main/java/main/ForChange.java
@@ -1,8 +1,12 @@
 package main;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class ForChange {
 
     public static void main(String[] args) {
+        JSONObject jsonObject = new JSONObject();
+        System.out.println(jsonObject.toJSONString());
         System.out.println("Hello github.");
     }
 
